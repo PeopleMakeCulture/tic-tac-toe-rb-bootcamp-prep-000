0ab91d55dc55ac8f1530f7b9029cf9a7d41d4b05
Jing Cao <jingcao.yale@gmail.com>
2018-07-16 20:39:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,7 +29,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, X_or_O)
+    move(board, index, sign)
   else
     turn(board)
   end
