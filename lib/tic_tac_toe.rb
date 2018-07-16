37e3b6f772dfe9da5a48cdf7bd5555a771d9c89e
Jing Cao <jingcao.yale@gmail.com>
2018-07-16 21:05:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  #If the turn count is an even number, the #current_player method should return "X", otherwise, it should return "O".
   turn_count(board) % 2 == 1? "X" : "O"
 end
 
