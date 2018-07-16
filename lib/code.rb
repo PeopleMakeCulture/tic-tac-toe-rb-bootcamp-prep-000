de62a1a009224b7f23cbaa2aed9aff096389a70b
Jing Cao <jingcao.yale@gmail.com>
2018-07-16 20:54:35 +0000

    automatically backed up by learn

--- lib/code.rb
+++ lib/code.rb
@@ -15,11 +15,10 @@ def turn(board)
   puts "Please enter 1-9:"
   user_number = gets.strip
   index = input_to_index(user_number)
-  if valid_move?(board,index) == false
+  if !(valid_move?(board,index)) 
     puts "Not a valid move. Try again."
     turn(board)
   else
     move(board, index, sign)
   end
 end
-
